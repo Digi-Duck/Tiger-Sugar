086589fae21651effc24331e2a3f6d37c5a1412d
cable <m2337733@gmail.com>
2023-08-31 00:14:59 +0800

    產品經銷 大約版型（尚未完成）

--- js/swiperJS.js
+++ js/swiperJS.js
@@ -2,7 +2,16 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button .swiper-button-next",
+      prevEl: ".swiper-button .swiper-button-prev",
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button2 .swiper-button-next",
+      prevEl: ".swiper-button2 .swiper-button-prev",
     },
   });
