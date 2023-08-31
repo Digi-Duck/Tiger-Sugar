ed42055dacf405f6bba167d5988b88dfee08c3c5
cable <m2337733@gmail.com>
2023-08-31 23:28:08 +0800

    swiper小錯誤修正

--- js/swiperJS.js
+++ js/swiperJS.js
@@ -15,3 +15,15 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button2 .swiper-button-prev",
     },
   });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button3 .swiper-button-next",
+      prevEl: ".swiper-button3 .swiper-button-prev",
+    },
+  });
+
+  
+document.querySelectorAll("#news .swiper-slide").style.margin="unset";
