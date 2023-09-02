5034290b99e46fd3842e592ea24269bee8412ab8
cable <m2337733@gmail.com>
2023-09-02 23:02:29 +0800

    我盡力了

--- js/swiperJS.js
+++ js/swiperJS.js
@@ -18,22 +18,42 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   var swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
       nextEl: ".swiper-button2 .swiper-button-next",
       prevEl: ".swiper-button2 .swiper-button-prev",
     },
+    breakpoints: {
+      481: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
   });
 
   var swiper3 = new Swiper(".mySwiper3", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 48,
     loop: true,
     navigation: {
       nextEl: ".swiper-button3 .swiper-button-next",
       prevEl: ".swiper-button3 .swiper-button-prev",
     },
+    breakpoints: {
+      481: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
   });
 
 
