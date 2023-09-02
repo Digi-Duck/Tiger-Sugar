d8ecd7c15c3dad5038c62563e577e8a03454d289
cable <m2337733@gmail.com>
2023-09-02 22:16:09 +0800

    第一部分rwd完成

--- js/swiperJS.js
+++ js/swiperJS.js
@@ -1,10 +1,20 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".swiper-button .swiper-button-next",
       prevEl: ".swiper-button .swiper-button-prev",
     },
+    breakpoints: {
+      481: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
   });
 
   var swiper2 = new Swiper(".mySwiper2", {
