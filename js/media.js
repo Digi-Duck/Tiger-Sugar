465190eb2adee02ad5262f4fde9af345c0447b7f
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-09-03 09:33:59 +0800

    用link整理完連結了

---
+++ js/media.js
@@ -0,0 +1,31 @@
+
+
+var mediaSwiper = new Swiper(".media-swiper", {
+    navigation: {
+        nextEl: ".media-button-next",
+        prevEl: ".media-button-prev",
+    },
+});
+
+
+let toggleButtons = document.querySelectorAll('.volumn');
+
+        toggleButtons.forEach(element => {
+            element.addEventListener('click', function () {
+                let targetVideo = element.previousElementSibling;
+
+                // 開啟
+                if (targetVideo.muted) {
+                    targetVideo.muted = false;
+                    element.classList.remove("fa-volume-xmark");
+                    element.classList.add("fa-volume-high");
+
+                }
+                // 靜音
+                else {
+                    targetVideo.muted = true;
+                    element.classList.remove("fa-volume-high");
+                    element.classList.add("fa-volume-xmark");
+                }
+            })
+        });
