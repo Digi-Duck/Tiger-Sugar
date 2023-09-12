efb46d5f2dfff84e30504bc7adcef5b6bb8f0cff
pushTest <140990768+sp1daTomm@users.noreply.github.com>
2023-09-12 19:25:17 +0800

    revisingDropDownNav

--- js/popwindow.js
+++ js/popwindow.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".my-swiper", {
+var swiper = new Swiper(".my-pop-swiper", {
     loop: true,
     spaceBetween: 4,
     slidesPerView: 4,
@@ -7,7 +7,7 @@ var swiper = new Swiper(".my-swiper", {
     direction: 'vertical',
 });
 
-var swiper2 = new Swiper(".my-swiper-sub", {
+var swiperSub = new Swiper(".my-pop-swiper-sub", {
     loop: true,
     spaceBetween: 4,
     navigation: {
@@ -19,29 +19,11 @@ var swiper2 = new Swiper(".my-swiper-sub", {
     },
 });
 
-// var swiperProduct = new Swiper("#pop-window .mySwiper-product", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     navigation: {
-//         nextEl: ".swiper-button-product .swiper-button-next",
-//         prevEl: ".swiper-button-product .swiper-button-prev",
-//     },
-//     breakpoints: {
-//         992: {
-//             slidesPerView: 2,
-//             spaceBetween: 20,
-//         },
-//         1400: {
-//             slidesPerView: 3,
-//             spaceBetween: 12,
-//         }
-//     }
-// });
 
 
 // let appendNumber = 3;
 // let prependNumber = 1;
-// const swiper = new Swiper('.swiper', {
+// const swiper = new Swiper('.my-pop-swiper', {
 //     slidesPerView: 3,
 //     centeredSlides: true,
 //     spaceBetween: 30,
@@ -65,5 +47,61 @@ var swiper2 = new Swiper(".my-swiper-sub", {
 // });
 
 
+var swiperCommidity = new Swiper(".pop-window-bottom-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-product .swiper-button-next",
+        prevEl: ".swiper-button-product .swiper-button-prev",
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1400: {
+            slidesPerView: 3,
+            spaceBetween: 12,
+        }
+    }
+});
 
+//  table選單
 
+const navDropDownLeft = document.querySelector('.drop-down-content'); 
+const navDropDownMiddle = document.querySelector('.drop-down-warn'); 
+const navDropDownRight = document.querySelector('.drop-down-video'); 
+const contentLeft = document.querySelector('.content-left'); 
+const contentMiddle = document.querySelector('.content-middle'); 
+const contentRight = document.querySelector('.content-right'); 
+
+contentLeft.style.display = 'block';
+navDropDownLeft.addEventListener('click', () => {
+    contentLeft.style.display = 'block';
+    contentMiddle.style.display = 'none';
+    contentRight.style.display = 'none';
+
+    navDropDownLeft.classList.add('active');
+    navDropDownMiddle.classList.remove('active');
+    navDropDownRight.classList.remove('active');
+});
+
+navDropDownMiddle.addEventListener('click', () => {
+    contentLeft.style.display = 'none';
+    contentMiddle.style.display = 'block';
+    contentRight.style.display = 'none';
+
+    navDropDownLeft.classList.remove('active');
+    navDropDownMiddle.classList.add('active');
+    navDropDownRight.classList.remove('active');
+});
+
+navDropDownRight.addEventListener('click', () => {
+    contentLeft.style.display = 'none';
+    contentMiddle.style.display = 'none';
+    contentRight.style.display = 'block';
+
+    navDropDownLeft.classList.remove('active');
+    navDropDownMiddle.classList.remove('active');
+    navDropDownRight.classList.add('active');
+});
