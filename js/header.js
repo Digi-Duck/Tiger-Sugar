4eb859550b28c7121f18add481ea20b5e3314207
Chiuu7711 <140990680+Chiuu7711@users.noreply.github.com>
2023-09-20 15:59:17 +0800

    語言swiper嘗試-更新
    語言swiper嘗試-更新

--- js/header.js
+++ js/header.js
@@ -7,7 +7,7 @@ navToggler.addEventListener('click', function () {
 
 var swiper = new Swiper(".header-swiper-container", {
   navigation: {
-    nextEl: ".header-button-next",
-    prevEl: ".header-button-prev",
+    nextEl: ".swiper-btn-right",
+    prevEl: ".swiper-btn-left",
   },
 });
