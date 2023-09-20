bf64d8d4ad8b852d2819374c40a6bdb0823b10d7
pushTest <140990768+sp1daTomm@users.noreply.github.com>
2023-09-20 09:33:57 +0800

    addPopWindowOverlay

--- js/distribution-test.js
+++ js/distribution-test.js
@@ -1,13 +1,13 @@
 function closePopup(event) {
-    event.preventDefault(); // 防止默認連接操作
     document.querySelector('.im-pop-window').style.display = 'none';
+    document.querySelector('.window-overlay').style.display = 'none';
 }
 
 const openPopWindows = document.querySelectorAll('.open-pop-window');
 openPopWindows.forEach(function(btn) {
     btn.addEventListener('click', function (event) {
-        event.preventDefault();
         document.querySelector('.im-pop-window').style.display ='block';
+        document.querySelector('.window-overlay').style.display = 'block';
         console.log('123');
     });
 });
@@ -15,19 +15,8 @@ openPopWindows.forEach(function(btn) {
 const closePopWindows = document.querySelectorAll('.pop-window-close');
 closePopWindows.forEach(function(btn) {
     btn.addEventListener('click', function (event) {
-        event.preventDefault();
         document.querySelector('.im-pop-window').style.display ='none';
-        console.log('321');
+        document.querySelector('.window-overlay').style.display = 'none';
+        console.log('event.target');
     });
 });
-
-// function openPopup(event) {
-//     event.preventDefault(); // 防止默認連接操作
-//     document.querySelector('.im-pop-window').style.display = 'block';
-//     console.log('123');
-// }
-
-// function closePopup(event) {
-//     event.preventDefault(); // 防止默認連接操作
-//     document.querySelector('.im-pop-window').style.display = 'none';
-// }
