58a27b532a95d8fff1c607cf1a1f527de1ce959d
cable <m2337733@gmail.com>
2023-09-22 11:16:51 +0800

    新增index.js

--- js/index.js
+++ js/index.js
@@ -55,28 +55,3 @@ var swiperNews = new Swiper("#news .mySwiper-news", {
     }
   }
 });
-
-var swiperDistributionContainer = new Swiper("#distribution-container .mySwiper-distribution-container", {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  navigation: {
-    nextEl: ".swiper-button-distribution-container .swiper-button-next",
-    prevEl: ".swiper-button-distribution-container .swiper-button-prev",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 0,
-    },
-    415: {
-      slidesPerView: 3,
-      spaceBetween: 0,
-    },
-    295: {
-      slidesPerView: 2,
-      spaceBetween: 0,
-    },
-  }
-});
-
-
