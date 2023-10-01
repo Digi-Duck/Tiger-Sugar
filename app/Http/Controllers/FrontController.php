4e943611affe447cc34cd714a0b7a504100a970b
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-10-01 23:48:41 +0800

    front資料夾改名frontend

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,24 +6,23 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index()
-    {
-        return view('front.index');
+    public function index() {
+        return view('frontend.index');
     }
 
-    public function distribution(){
-        return view('front.distribution');
+    public function distribution() {
+        return view('frontend.distribution');
     }
 
-    public function distributionConfirm(){
-        return view('front.distribution-confirm');
+    public function distributionConfirm() {
+        return view('frontend.distribution-confirm');
     }
 
-    public function franchisee(){
-        return view('front.franchisee');
+    public function franchisee() {
+        return view('frontend.franchisee');
     }
 
-    public function franchiseeForm(){
-        return view('front.franchisee-form');
+    public function franchiseeForm() {
+        return view('frontend.franchisee-form');
     }
 }
--- routes/frontend.php
+++ routes/frontend.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 // 前端路由
-Route::get('/index', [FrontController::class, 'index'])->name('front.index'); //首頁
+Route::get('/', [FrontController::class, 'index'])->name('front.index'); //首頁
 Route::get('/distribution', [FrontController::class, 'distribution'])->name('front.distribution'); //產品經銷頁
 Route::get('/distribution-confirm', [FrontController::class, 'distributionConfirm'])->name('front.distribution_confirm'); //經銷確認頁
 Route::get('/franchisee', [FrontController::class, 'franchisee'])->name('front.franchisee'); //加盟說明頁
 Route::get('/franchisee-form', [FrontController::class, 'franchiseeForm'])->name('front.franchisee_form'); //加盟表單頁
 
 //header跳轉到首頁各section的路由
-Route::get('index#link-about', [FrontController::class, 'indexToAbout'])->name('front.index.about'); //首頁的關於我們section
-Route::get('index#link-distribution', [FrontController::class, 'indexToDistribution'])->name('front.index.distribution'); //首頁的產品經銷section
-Route::get('index#link-classic', [FrontController::class, 'indexToClassic'])->name('front.index.classic'); //首頁的熱門經典section
-Route::get('index#link-media', [FrontController::class, 'indexToMedia'])->name('front.index.media'); //首頁的媒體露出section
-Route::get('index#link-franchisee', [FrontController::class, 'indexToFranchisee'])->name('front.index.franchisee'); //首頁的加盟專區section
+Route::get('#link-about', [FrontController::class, 'indexToAbout'])->name('front.index.about'); //首頁的關於我們section
+Route::get('#link-distribution', [FrontController::class, 'indexToDistribution'])->name('front.index.distribution'); //首頁的產品經銷section
+Route::get('#link-classic', [FrontController::class, 'indexToClassic'])->name('front.index.classic'); //首頁的熱門經典section
+Route::get('#link-media', [FrontController::class, 'indexToMedia'])->name('front.index.media'); //首頁的媒體露出section
+Route::get('#link-franchisee', [FrontController::class, 'indexToFranchisee'])->name('front.index.franchisee'); //首頁的加盟專區section
