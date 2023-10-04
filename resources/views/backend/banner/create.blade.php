24c052100bff4931749d055b8fdf8769f9b41ca4
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-10-04 16:49:54 +0800

    banner.create value可以正常切換
    註解先別砍，要去另一頁找同樣情況的問題

--- resources/views/backend/banner/create.blade.php
+++ resources/views/backend/banner/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.backend-template')
 
 @section('css')
-
 @endsection
 
 @section('main')
@@ -13,9 +12,9 @@
                         廣告橫幅管理-新增
                     </h4>
                     <div class="card-body">
-                        <form method="POST" action="{{route('back.banner.store')}}" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('back.banner.store') }}" enctype="multipart/form-data">
                             @csrf
-                            <input class="form-control" name="type" id="banner_type" value="圖片" required>
+                            <input class="form-control" name="type" id="banner_type" value="圖片" required hidden>
                             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 
 
@@ -28,30 +27,41 @@
 
 
                                 <li class="nav-item" role="presentation">
-                                    <a class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">圖片</a>
+                                    <a class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
+                                        data-bs-target="#pills-home" role="tab" aria-controls="pills-home"
+                                        aria-selected="true">圖片</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">影片</a>
+                                    <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
+                                        data-bs-target="#pills-profile" role="tab" aria-controls="pills-profile"
+                                        aria-selected="false">影片</a>
                                 </li>
                             </ul>
 
                             <hr>
 
                             <div class="tab-content" id="pills-tabContent">
-                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                                    aria-labelledby="pills-home-tab">
                                     <div class="form-group row">
                                         <label for="pc_image_url" class="col-2 col-form-label">上傳圖片</label>
                                         <div class="col-10">
-                                            <input type="file" class="form-control-file" id="pc_image_url" name="pc_image_url" required>
+                                            <input type="file" class="form-control-file" id="pc_image_url"
+                                                name="pc_image_url" required>
                                         </div>
-                                        <div class="col-12"><p class="text-danger">*注意：建議尺寸：1920 * 907 (px)</p></div>
+                                        <div class="col-12">
+                                            <p class="text-danger">*注意：建議尺寸：1920 * 907 (px)</p>
+                                        </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="mb_image_url" class="col-2 col-form-label">上傳圖片(手機版)</label>
                                         <div class="col-10">
-                                            <input type="file" class="form-control-file" id="mb_image_url" name="mb_image_url" required>
+                                            <input type="file" class="form-control-file" id="mb_image_url"
+                                                name="mb_image_url" required>
                                         </div>
-                                        <div class="col-12"><p class="text-danger">*注意：建議尺寸：420 * 640 (px)</p></div>
+                                        <div class="col-12">
+                                            <p class="text-danger">*注意：建議尺寸：420 * 640 (px)</p>
+                                        </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="image_alt" class="col-2 col-form-label">圖片替代文字(alt)</label>
@@ -68,27 +78,36 @@
                                     <div class="form-group row">
                                         <label for="link_url" class="col-2 col-form-label">另開視窗</label>
                                         <div class="col-10" style="padding-left: 35px">
-                                            <input class="form-check-input" type="checkbox" value="link_target" name="link_target" id="link_target">
+                                            <input class="form-check-input" type="checkbox" value="link_target"
+                                                name="link_target" id="link_target">
                                             <label class="form-check-label" for="link_target">
                                                 另開新視窗
                                             </label>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                                <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                                    aria-labelledby="pills-profile-tab">
                                     <div class="form-group row">
                                         <label for="pc_video_url" class="col-2 col-form-label">設定影片連結<br>(Youtube連結)</label>
                                         <div class="col-10">
-                                            <input type="text" class="form-control" id="pc_video_url" name="pc_video_url">
+                                            <input type="text" class="form-control" id="pc_video_url"
+                                                name="pc_video_url">
                                         </div>
-                                        <div class="col-12"><p class="text-danger">*注意：建議尺寸：1920 * 907 (px)</p></div>
+                                        <div class="col-12">
+                                            <p class="text-danger">*注意：建議尺寸：1920 * 907 (px)</p>
+                                        </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="mb_video_url" class="col-2 col-form-label">設定影片連結<br>(Youtube連結)</label>
+                                        <label for="mb_video_url"
+                                            class="col-2 col-form-label">設定影片連結<br>(Youtube連結)</label>
                                         <div class="col-10">
-                                            <input type="text" class="form-control" id="mb_video_url" name="mb_video_url">
+                                            <input type="text" class="form-control" id="mb_video_url"
+                                                name="mb_video_url">
                                         </div>
-                                        <div class="col-12"><p class="text-danger">*注意：建議尺寸：420 * 640 (px)</p></div>
+                                        <div class="col-12">
+                                            <p class="text-danger">*注意：建議尺寸：420 * 640 (px)</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -98,7 +117,8 @@
                             <div class="form-group row">
                                 <label for="sort" class="col-2 col-form-label">權重</label>
                                 <div class="col-10">
-                                    <input type="number" class="form-control" id="sort" name="sort" required value="0" min="0" max="999">
+                                    <input type="number" class="form-control" id="sort" name="sort" required
+                                        value="0" min="0" max="999">
                                 </div>
                             </div>
                             <hr>
@@ -112,13 +132,20 @@
 @endsection
 
 @section('js')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
-        $('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
-            if($(e.target).attr('id') == 'pills-home-tab'){
-                document.querySelector("#banner_type").setAttribute('value','圖片');
-            }else{
-                document.querySelector("#banner_type").setAttribute('value','影片');
+        let img = document.querySelector('#pills-home-tab');
+        let mov = document.querySelector('#pills-profile-tab');
+        let type = document.querySelector('#banner_type');
+        img.addEventListener('click', function() {
+            if (type.value === '影片') {
+                type.value = '圖片'
+                console.log(type.value);
+            }
+        })
+        mov.addEventListener('click', function() {
+            if (type.value === '圖片') {
+                type.value = '影片'
+                console.log(type.value);
             }
         })
     </script>
