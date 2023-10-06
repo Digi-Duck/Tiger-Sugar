c9dc6138c15a007fc5a35b51598d40db1250d3ed
cbc123459822 <141071332+cbc123459822@users.noreply.github.com>
2023-10-06 16:02:18 +0800

    social 能顯示 但 del edit 有問題

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Social;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use App\Services\FileService;
 
 class SocialController extends Controller
 {
@@ -16,15 +17,14 @@ class SocialController extends Controller
     //     $this->edit = 'admin.social.edit';
     // }
 
+    public function __construct(protected FileService $fileService)
+    {
+    }
+
     public function index()
     {
         $lists = Social::all();
-        return view('backend.social.index',compact('lists'));
-    }
-
-    public function create_page()
-    {
-        return view('backend.social.create');
+        return view('backend.social.index', compact('lists'));
     }
 
     public function create()
@@ -34,108 +34,118 @@ class SocialController extends Controller
 
     public function store(Request $request)
     {
-        $new_record = new Social();
-        //embed or social
-        $new_record -> type = $request->type;
-        $new_record -> sort  = $request->sort;
 
-        //embed
-        $new_record -> embed_link = $request->embed_link;
-        $new_record -> embed_name = $request->embed_name;
-
-        //social
-        $new_record -> user_name = $request->user_name;
-        $new_record -> user_account = $request->user_account;
-        $new_record -> social_icon = $request->social_icon;
-        $new_record -> social_icon_color = $request->social_icon_color;
-        $new_record -> link_title = $request->link_title;
-        $new_record -> link_href = $request->link_href;
-        $new_record -> link_target = $request->link_target;
-        $new_record -> social_info = $request->social_info;
-        $new_record -> post_date = $request->post_date;
+        $social = Social::create([
+            'type' => $request->type,
+            'sort' => $request->sort,
+            'embed_name' => $request->embed_name,
+            'embed_link' => $request->embed_link,
+            'user_icon' => $request->user_icon,
+            'user_name' => $request->user_name,
+            'user_account' => $request->user_account,
+            'social_icon' => $request->social_icon,
+            'social_icon_color' => $request->social_icon_color,
+            'link-title' => $request->link_title,
+            'link-href' => $request->link_href,
+            'link-target' => $request->link_target,
+            'social_info' => $request->social_info,
+            'post_date' => $request->post_date,
+        ]);
 
 
-        if($request->hasFile('user_photo')){
-            $new_record->user_photo = $this->upload_file($request->file('user_photo'));
+        $type = $request->type;
+
+        if ($type == '圖片') {
+            $request->validate([
+                'user_photo' => 'image',
+                'main_photo' => 'image',
+            ]);
+
+            $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
+            $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
+            $social->update([
+                'user_photo' => $userimg,
+                'main_photo' => $mainimg,
+            ]);
         }
-        if($request->hasFile('main_photo')){
-            $new_record->main_photo = $this->upload_file($request->file('main_photo'));
-        }
 
-        $new_record -> save();
-        return redirect('back.social.index')->with('message','新增成功!');
+        return redirect('back/social/index')->with('message', '新增成功!');
     }
 
     public function edit($id)
     {
         $info = Social::find($id);
-        return view('backend.store.edit',compact('info','id'));
+        return view('back/social/edit', compact('info', 'id'));
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
-        $social = Social::find($id);
-        //embed or social
-        $social -> type = $request->type;
-        $social -> sort  = $request->sort;
+        $social = Social::find($id)([
+            'type' => $request->type,
+            'sort' => $request->sort,
+            'embed_name' => $request->embed_name,
+            'embed_link' => $request->embed_link,
+            'user_icon' => $request->user_icon,
+            'user_name' => $request->user_name,
+            'user_account' => $request->user_account,
+            'social_icon' => $request->social_icon,
+            'social_icon_color' => $request->social_icon_color,
+            'link-title' => $request->link_title,
+            'link-href' => $request->link_href,
+            'link-target' => $request->link_target,
+            'social_info' => $request->social_info,
+            'post_date' => $request->post_date,
+        ]);
 
-        //embed
-        $social -> embed_link = $request->embed_link;
-        $social -> embed_name = $request->embed_name;
+        $type = $request->type;
 
-        //social
-        $social -> user_name = $request->user_name;
-        $social -> user_account = $request->user_account;
-        $social -> social_icon = $request->social_icon;
-        $social -> social_icon_color = $request->social_icon_color;
-        $social -> link_title = $request->link_title;
-        $social -> link_href = $request->link_href;
-        $social -> link_target = $request->link_target;
-        $social -> social_info = $request->social_info;
-        $social -> post_date = $request->post_date;
+        if ($type == '圖片') {
+            $request->validate([
+                'user_photo' => 'image',
+                'main_photo' => 'image',
+            ]);
 
-        if($request->hasFile('user_photo')){
-            $this->delete_file($social->user_photo);
-            $social->user_photo = $this->upload_file($request->file('user_photo'));
+            $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
+            $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
+            $social->update([
+                'user_photo' => $userimg,
+                'main_photo' => $mainimg,
+            ]);
         }
-        if($request->hasFile('main_photo')){
-            $this->delete_file($social->main_photo);
-            $social->main_photo = $this->upload_file($request->file('main_photo'));
-        }
 
-        $social -> save();
-        return redirect('/admin/social')->with('message','更新成功!');
+        return redirect('back/social/index')->with('message', '新增成功!');
     }
 
-    public function delete($id)
+    public function delete(Request $request, $id)
     {
         $social = Social::find($id);
+
+        // Assuming that the deleteUpload method deletes the file
+        $this->fileService->deleteUpload($social->user_photo);
+        $this->fileService->deleteUpload($social->main_photo);
+
         $social->delete();
-        return redirect('/admin/social')->with('message','刪除成功!');
+
+        return redirect('/back/social/index')->with('message', '刪除成功!');
     }
 
     //上傳檔案
-    public function upload_file($file){
-        $allowed_extensions =["png", "jpg", "gif", "PNG", "JPG", "GIF","jpeg","JPEG"];
-        if ($file->getClientOriginalExtension() &&
-            !in_array($file->getClientOriginalExtension(), $allowed_extensions))
-        {
-            return redirect()->back()->with('message','僅接受.jpg, .png, .gif, .jepg格式檔案!');
-        }
-        $extension = $file->getClientOriginalExtension();
-        $destinationPath = public_path() . '/summernote_upload/';
-        $original_filename = $file->getClientOriginalName();
+    // public function upload_file($file){
+    //     $allowed_extensions =["png", "jpg", "gif", "PNG", "JPG", "GIF","jpeg","JPEG"];
+    //     if ($file->getClientOriginalExtension() &&
+    //         !in_array($file->getClientOriginalExtension(), $allowed_extensions))
+    //     {
+    //         return redirect()->back()->with('message','僅接受.jpg, .png, .gif, .jepg格式檔案!');
+    //     }
+    //     $extension = $file->getClientOriginalExtension();
+    //     $destinationPath = public_path() . '/summernote_upload/';
+    //     $original_filename = $file->getClientOriginalName();
 
-        $filename = $file->getFilename() . '.' . $extension;
-        $url = '/summernote_upload/' . $filename;
+    //     $filename = $file->getFilename() . '.' . $extension;
+    //     $url = '/summernote_upload/' . $filename;
 
-        $file->move($destinationPath, $filename);
+    //     $file->move($destinationPath, $filename);
 
-        return $url;
-    }
-
-    //刪除檔案
-    public function delete_file($path){
-        File::delete(public_path().$path);
-    }
+    //     return $url;
+    // }
 }
