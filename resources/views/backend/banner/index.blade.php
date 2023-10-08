ab50fc99486ec9947a6ff0ce8f809a6af8b4c535
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-10-09 00:27:30 +0800

    清除不必要的東西

--- resources/views/backend/banner/index.blade.php
+++ resources/views/backend/banner/index.blade.php
@@ -109,11 +109,6 @@
 @endsection
 
 @section('js')
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
     <script>
         function changePages() {
             let pageSelect = document.querySelector('#page-select');
