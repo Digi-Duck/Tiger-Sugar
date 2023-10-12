306c55ebac5461a4e0a8abd07b7e048977a7a2a1
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-10-12 14:10:57 +0800

    product修復完成

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -17,10 +17,30 @@ class ProductsController extends Controller
     {
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $lists = Products::orderBy('sort','desc')->get();
-        return view('backend.products.index', compact('lists'));
+        $lists = Products::query();
+        $keyword = $request->keyword ?? '';
+        $page_numbers = $request->page_numbers;
+        $page = $request->page;
+        $count = $lists->count();
+
+        if ($request->filled('keyword')) {
+            $lists->where('image_alt', 'like', "%{$keyword}%")
+                ->orwhere('sort', 'like', "%{$keyword}%");
+        }
+
+        if ($page_numbers == null) {
+            $page_numbers = 10;
+        }
+
+        if ($page == null) {
+            $page = 1;
+        }
+        $lists->orderBy('sort', 'asc');
+        $lists = $lists->paginate($page_numbers);
+        $lists->appends(compact('lists', 'keyword', 'page_numbers'));
+        return view('backend.products.index', compact('lists', 'keyword', 'page_numbers', 'page', 'count'));
     }
 
     public function create()
