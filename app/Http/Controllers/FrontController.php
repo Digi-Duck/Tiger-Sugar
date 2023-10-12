e2e35ed8fa07f3e04aaa822b89d7fde27b5408c6
cable <m2337733@gmail.com>
2023-10-12 23:48:18 +0800

    social測試 / 並刪掉沒用的東西

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
 use App\Models\BlogNews;
 use App\Models\DrinkType;
 use App\Models\Media;
 use App\Models\Products;
+use App\Models\Social;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index() {
+        $banners = Banner::orderBy('sort','asc')->get();
+        $social = Social::orderBy('sort', 'asc')->get();
         $blognews = BlogNews::orderBy('sort','asc')->get();
         $medias = Media::orderBy('sort','asc')->get();
         $drink_types = DrinkType::orderBy('sort','asc')->get();
         $products = Products::orderBy('sort', 'asc')->take(6)->get();
-        return view('frontend.index',compact('blognews','medias', 'drink_types', 'products'));
+        return view('frontend.index',compact('blognews','medias', 'drink_types', 'products', 'social', 'banners'));
     }
 
     public function distribution() {
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -13,36 +13,36 @@
         <section id="banner">
             <div class="swiper mySwiper banner-swiper">
                 <div class="swiper-wrapper">
-                  <div class="swiper-slide">
-                    <div class="container">
-                        <div class="move-bg bg"></div>
-                        <div class="move-bg bg-cover"></div>
-                        <div class="move-bg bg-left-tiger"></div>
-                        <div class="move-bg bg-left-milk-shadow"></div>
-                        <div class="move-bg bg-left-milk"></div>
-                        <div class="move-bg bg-left-drinks"></div>
-                        <div class="move-bg bg-right-drinks"></div>
-                        <div class="move-bg bg-right-milk"></div>
-                        <div class="move-bg bg-center"></div>
-                        <h2 class="banner-text">Brave As A Tiger</h2>
-                        <div class="trans-cover"></div>
+                    <div class="swiper-slide">
+                        <div class="container">
+                            <div class="move-bg bg"></div>
+                            <div class="move-bg bg-cover"></div>
+                            <div class="move-bg bg-left-tiger"></div>
+                            <div class="move-bg bg-left-milk-shadow"></div>
+                            <div class="move-bg bg-left-milk"></div>
+                            <div class="move-bg bg-left-drinks"></div>
+                            <div class="move-bg bg-right-drinks"></div>
+                            <div class="move-bg bg-right-milk"></div>
+                            <div class="move-bg bg-center"></div>
+                            <h2 class="banner-text">Brave As A Tiger</h2>
+                            <div class="trans-cover"></div>
+                        </div>
                     </div>
-                  </div>
-                  {{-- for each 用 --}}
-                  <div class="swiper-slide">
-                    {{-- <img src="{{ asset('frontend-img/distribution-img/pop-window/16508571967e7757b1e12abcb736ab9a754ffb617a.jpg') }}" alt=""> --}}
-                  </div>
-                  <div class="swiper-slide">
-                    {{-- <iframe src="{{ asset('frontend-img/index-img/media/phpEGFGi8.mp4') }}" frameborder="0"></iframe> --}}
-                  </div>
-                  <div class="swiper-slide">
-                    {{-- <iframe src="https://www.youtube.com/embed/eHAyE76wog0?si=i8xewDu9a9DXQu2J" title="YouTube video player" frameborder="0"></iframe> --}}
-                  </div>
+                    {{-- for each 用 --}}
+                    <div class="swiper-slide">
+                        {{-- <img src="{{ asset('frontend-img/distribution-img/pop-window/16508571967e7757b1e12abcb736ab9a754ffb617a.jpg') }}" alt=""> --}}
+                    </div>
+                    <div class="swiper-slide">
+                        {{-- <iframe src="{{ asset('frontend-img/index-img/media/phpEGFGi8.mp4') }}" frameborder="0"></iframe> --}}
+                    </div>
+                    <div class="swiper-slide">
+                        {{-- <iframe src="https://www.youtube.com/embed/eHAyE76wog0?si=i8xewDu9a9DXQu2J" title="YouTube video player" frameborder="0"></iframe> --}}
+                    </div>
 
                 </div>
                 <div class="swiper-button-next"></div>
                 <div class="swiper-button-prev"></div>
-              </div>
+            </div>
 
         </section>
 
@@ -130,10 +130,48 @@
                 <!-- Swiper -->
                 <div class="swiper mySwiper-social">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide"><iframe class="card-body"
-                                src="https://www.instagram.com/p/Cq9ZWd9hUSw/embed/?cr=1&amp;v=14&amp;wp=696&amp;rd=https%3A%2F%2Ftigersugar.com&amp;rp=%2F#%7B%22ci%22%3A0%2C%22os%22%3A715.5%2C%22ls%22%3A418.89999997615814%2C%22le%22%3A430%7D"
-                                allowtransparency="true" allowfullscreen="true" frameborder="0"
-                                data-instgrm-payload-id="instagram-media-payload-0" scrolling="no"></iframe></div>
+                        @foreach ($social as $single_social)
+                            @if ($single_social->type == 'embed')
+                                <div class="swiper-slide"><iframe class="card-body" src="{{ $single_social->embed_link }}"
+                                        allowtransparency="true" allowfullscreen="true" frameborder="0"
+                                        data-instgrm-payload-id="instagram-media-payload-0" scrolling="no"></iframe></div>
+                            @else
+                                <div class="swiper-slide">
+                                    <div class="card-background">
+                                        <div class="post-userinfo">
+                                            <div class="float-left">
+                                                <span class="post-division"
+                                                    style="background-image: url('{{ $single_social->user_photo }}')"></span>
+                                            </div>
+                                            <div class="float-left">
+                                                <span class="user-name">{{ $single_social->user_name }}</span>
+                                                <span class="user-name2">{{ $single_social->user_account }}</span>
+                                            </div>
+                                            <div class="float-right"
+                                                style="color:{{ $single_social->social_icon_color }}">
+                                                {!! $single_social->social_icon !!}
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="card-img"
+                                            style="background-image:url('{{ $single_social->main_photo }}');"></div>
+                                        <div class="px-3">
+                                            <div class="post-link">
+                                                <a href="{{ $single_social->link_href }}">{{ $single_social->link_title }}</a>
+                                            </div>
+                                        </div>
+                                        <div class="px-3">
+                                            <div class="post-text">
+                                                {{ $single_social->social_info }}
+                                            </div>
+                                        </div>
+                                        <div class="px-3">
+                                            <div class="post-date">{{ $single_social->post_date }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
                         <div class="swiper-slide"><iframe class="card-body"
                                 src="https://www.instagram.com/p/Ck3jKhFAVqQ/embed/?cr=1&amp;v=14&amp;wp=696&amp;rd=https%3A%2F%2Ftigersugar.com&amp;rp=%2F#%7B%22ci%22%3A1%2C%22os%22%3A717.3000000119209%2C%22ls%22%3A418.89999997615814%2C%22le%22%3A430%7D"
                                 allowtransparency="true" allowfullscreen="true" frameborder="0"
@@ -188,38 +226,36 @@
                     <div class="swiper mySwiper-product">
                         <div class="swiper-wrapper">
                             @foreach ($products as $product)
-                            <div class="swiper-slide">
-                                <div class="direction-body">
-                                    <div class="img-w-h-border-img">
-                                        <img class="product-img"
-                                            src="{{ $product->img }}"
-                                            alt="產品圖片">
-                                        <img class="ask-icon" data-product="{{ $product->id }}"
-                                            src="{{ asset('./frontend-img/index-img/distribution/add_for_ask.svg') }}"
-                                            alt="黃色加入以詢問">
-                                        <div class="product-img-hover">
-                                            <a href="" class="cursor-p">
-                                                <img class="ask-icon-hover" data-product="{{ $product->id }}"
-                                                    src="{{ asset('./frontend-img/index-img/distribution/add_for_ask_hover.svg') }}"
-                                                    alt="黃色加入以詢問">
-                                            </a>
-                                            <a href={{ route('front.distribution') }} class="commodity-more-button btn"
-                                                title="更多商品">
-                                                MORE
-                                            </a>
+                                <div class="swiper-slide">
+                                    <div class="direction-body">
+                                        <div class="img-w-h-border-img">
+                                            <img class="product-img" src="{{ $product->img }}" alt="產品圖片">
+                                            <img class="ask-icon" data-product="{{ $product->id }}"
+                                                src="{{ asset('./frontend-img/index-img/distribution/add_for_ask.svg') }}"
+                                                alt="黃色加入以詢問">
+                                            <div class="product-img-hover">
+                                                <a href="" class="cursor-p">
+                                                    <img class="ask-icon-hover" data-product="{{ $product->id }}"
+                                                        src="{{ asset('./frontend-img/index-img/distribution/add_for_ask_hover.svg') }}"
+                                                        alt="黃色加入以詢問">
+                                                </a>
+                                                <a href={{ route('front.distribution') }}
+                                                    class="commodity-more-button btn" title="更多商品">
+                                                    MORE
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="commodity-title">
+                                            {{ $product->title_zh }}
+                                        </div>
+                                        <div class="commodity-title-english">
+                                            {{ $product->title_en }}
+                                        </div>
+                                        <div class="commodity-sort">
+                                            {{ $product->productsType->tw_name }}|{{ $product->productsType->en_name }}
                                         </div>
                                     </div>
-                                    <div class="commodity-title">
-                                        {{ $product->title_zh }}
-                                    </div>
-                                    <div class="commodity-title-english">
-                                        {{ $product->title_en }}
-                                    </div>
-                                    <div class="commodity-sort">
-                                        {{ $product->productsType->tw_name }}|{{ $product->productsType->en_name }}
-                                    </div>
                                 </div>
-                            </div>
                             @endforeach
                             <div class="swiper-slide">
                                 <div class="direction-body">
@@ -442,35 +478,35 @@
                         </div>
                         <div class="menu-up">
                             @foreach ($drink_types as $drink_type)
-                            <div data-aos="fade-up" data-aos-delay="{{ $loop->index*300 }}">
-                                <div class="menu-type">
-                                    <div class="up">
-                                        <h4 class="color-white">{{ $drink_type->type_name }}</h4>
-                                        @if ($drink_type->type_info != '')
-                                        <span class="menu-type-text">{{ $drink_type->type_info }}</span>
-                                        @endif
-                                    </div>
-                                    <div class="ul-group">
-                                        <ul class="gold-border">
-                                            @foreach ($drink_type->drink as $drink)
-                                            <li>
-                                                <div class="menu-type-title">
-                                                    <div class="menu-text">{{ $drink->drink_name }}</div>
-                                                    <div class="small-dot">
-                                                        @if ($drink->cold == '1')
-                                                        <div class="cold"></div>
-                                                        @endif
-                                                        @if ($drink->hot == '1')
-                                                        <div class="hot"></div>
-                                                        @endif
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            @endforeach
-                                        </ul>
+                                <div data-aos="fade-up" data-aos-delay="{{ $loop->index * 300 }}">
+                                    <div class="menu-type">
+                                        <div class="up">
+                                            <h4 class="color-white">{{ $drink_type->type_name }}</h4>
+                                            @if ($drink_type->type_info != '')
+                                                <span class="menu-type-text">{{ $drink_type->type_info }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="ul-group">
+                                            <ul class="gold-border">
+                                                @foreach ($drink_type->drink as $drink)
+                                                    <li>
+                                                        <div class="menu-type-title">
+                                                            <div class="menu-text">{{ $drink->drink_name }}</div>
+                                                            <div class="small-dot">
+                                                                @if ($drink->cold == '1')
+                                                                    <div class="cold"></div>
+                                                                @endif
+                                                                @if ($drink->hot == '1')
+                                                                    <div class="hot"></div>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             @endforeach
                         </div>
                         <div class="menu-mid">
@@ -568,12 +604,12 @@
             <div class="swiper mySwiper media-swiper">
                 <div class="swiper-wrapper">
                     @foreach ($medias as $media)
-                    <div class="swiper-slide">
-                        <video class="video swiper-slide" autoplay loop muted id="video-real-case">
-                            <source src="{{ $media->link }}" type="video/mp4">
-                        </video>
-                        <i class="fa-solid fa-volume-xmark volumn"></i>
-                    </div>
+                        <div class="swiper-slide">
+                            <video class="video swiper-slide" autoplay loop muted id="video-real-case">
+                                <source src="{{ $media->link }}" type="video/mp4">
+                            </video>
+                            <i class="fa-solid fa-volume-xmark volumn"></i>
+                        </div>
                     @endforeach
                 </div>
                 <div class="swiper-button-next media-button-next"></div>
@@ -600,20 +636,20 @@
                 <div class="swiper mySwiper-news">
                     <div class="swiper-wrapper">
                         @foreach ($blognews as $blognew)
-                        <div class="swiper-slide">
-                            <a target="_blank" href="{{ $blognew->link }}" title="波波黛莉夏季新推連結">
-                                <div class="outer-card">
-                                    <div class="card">
+                            <div class="swiper-slide">
+                                <a target="_blank" href="{{ $blognew->link }}" title="波波黛莉夏季新推連結">
+                                    <div class="outer-card">
+                                        <div class="card">
                                             <img class="news-img" src="{{ $blognew->main_photo }}" alt="">
-                                        <div class="card-body">
-                                            <div class="blog-name">{{ $blognew->author }}</div>
-                                            <div class="card-title">{{ $blognew->title }}</div>
-                                            <div class="card-content">{{ $blognew->info }}</div>
+                                            <div class="card-body">
+                                                <div class="blog-name">{{ $blognew->author }}</div>
+                                                <div class="card-title">{{ $blognew->title }}</div>
+                                                <div class="card-content">{{ $blognew->info }}</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
+                                </a>
+                            </div>
                         @endforeach
                     </div>
                 </div>
