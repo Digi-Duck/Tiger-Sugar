62e5b6cccea7ca6a32b8313d9882e4517d6996c8
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-10-13 10:57:18 +0800

    social 調整完畢

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -66,12 +66,28 @@ class SocialController extends Controller
             'post_date' => $request->post_date,
         ]);
 
-        $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
-        $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
-        $social->update([
-            'user_photo' => $userimg,
-            'main_photo' => $mainimg,
-        ]);
+        $type = $request->type;
+
+        if ($type == 'custom') {
+            $request->validate([
+                'user_photo' => 'image',
+                'main_photo' => 'image',
+            ]);
+
+            $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
+            $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
+            $social->update([
+                'user_photo' => $userimg,
+                'main_photo' => $mainimg,
+            ]);
+        }
+
+        // $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
+        // $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
+        // $social->update([
+        //     'user_photo' => $userimg,
+        //     'main_photo' => $mainimg,
+        // ]);
 
         // $type = $request->type;
 
@@ -89,19 +105,7 @@ class SocialController extends Controller
         //     ]);
         // }
 
-        // if ($type == 'custom') {
-        //     // $request->validate([
-        //     //     'user_photo' => 'image',
-        //     //     'main_photo' => 'image',
-        //     // ]);
 
-        //     $userimg = $this->fileService->imgUpload($request->file('user_photo'), 'social-userimg');
-        //     $mainimg = $this->fileService->imgUpload($request->file('main_photo'), 'social-mainimg');
-        //     $social->update([
-        //         'user_photo' => $userimg,
-        //         'main_photo' => $mainimg,
-        //     ]);
-        // }
 
         return redirect(route('back.social.index'))->with('message', '新增成功!');
     }
