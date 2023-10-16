46bcc340f5b235218f652704b9488d760d0a9eaf
cbc123459822 <141071332+cbc123459822@users.noreply.github.com>
2023-10-16 09:46:16 +0800

    中文版 form reqired 完成

--- resources/views/frontend/franchisee-form.blade.php
+++ resources/views/frontend/franchisee-form.blade.php
@@ -23,46 +23,46 @@
             <form class="form main" action="{{route('front.franchisee_form.store')}}" method="POST">
                 @csrf
                 <div class="form-name">
-                    <label for="user_name">姓名｜Name</label>
-                    <input id="user_name" type="text" class="form-name-input" name="user_name">
+                    <label for="user_name">姓名｜Name <span>*</span> </label>
+                    <input id="user_name" type="text" class="form-name-input" placeholder="請輸入姓名" required name="user_name">
                 </div>
                 <div class="form-date">
-                    <label for="birth_day">出生年月日｜Date of birth</label>
-                    <input id="birth_day" type="date" class="form-date-input" name="birth_day">
+                    <label for="birth_day">出生年月日｜Date of birth <span>*</span> </label>
+                    <input id="birth_day" type="date" class="form-date-input" required name="birth_day">
                 </div>
                 <div class="form-email">
-                    <label for="email">電子信箱｜Email</label>
-                    <input id="email" type="text" class="form-email-input" name="email">
+                    <label for="email">電子信箱｜Email <span>*</span> </label>
+                    <input id="email" type="text" class="form-email-input" placeholder="請輸入電子郵件" required name="email">
                 </div>
                 <div class="form-phone">
-                    <label for="phone">聯絡電話｜Phone</label>
-                    <input id="phone" type="text" class="form-phone-input" name="phone">
+                    <label for="phone">聯絡電話｜Phone <span>*</span> </label>
+                    <input id="phone" type="text" class="form-phone-input" required name="phone">
                 </div>
                 <div class="form-address">
-                    <label for="address">通訊地址｜Address</label>
-                    <input id="address" type="text" class="form-address-input" name="address">
+                    <label for="address">通訊地址｜Address <span>*</span> </label>
+                    <input id="address" type="text" class="form-address-input" required name="address">
                 </div>
                 <div class="form-franchisee-type">
-                    <label for="franchisee_type">加盟方式｜franchisee type</label>
-                    <select id="franchisee_type" type="text" class="form-franchisee-type-input" name="franchisee_type">
+                    <label for="franchisee_type">加盟方式｜franchisee type <span>*</span> </label>
+                    <select id="franchisee_type" type="text" class="form-franchisee-type-input" required name="franchisee_type">
                         <option value disable selected>--請選擇--</option>
                         <option value="總代理">總代理 - General Agent</option>
                         <option value="加盟">加盟 - Franchise Store</option>
                     </select>
                 </div>
                 <div class="form-country">
-                    <label for="country">預定加盟城市|Store Address</label>
+                    <label for="country">預定加盟城市|Store Address <span>*</span> </label>
                     <div class="country-selector">
-                        <select data-region-id="where" class="form-country-input crs-country" name="country"
+                        <select data-region-id="where" class="form-country-input crs-country" required name="country"
                             data-region-id="country-region" data-default-value="Taiwan">
                         </select>
-                        <select id="where" class="form-country-input" name="capital" data-region-id="capital">
+                        <select id="where" class="form-country-input" required name="capital" data-region-id="capital">
                         </select>
                     </div>
                 </div>
                 <div class="form-country-city">
                     <input id="store_address" type="text" class="form-country-city-input" placeholder="Address"
-                        name="store_address">
+                        required name="store_address">
                 </div>
                 <div class="form-other">
                     <label for="other">其他｜other</label>
